7cb44d01d2be49f7e4a1b72ac152b1779215b00d
jun3-dev <jun.lee73@outlook.com>
2022-09-14 20:08:11 +1000

    Started on the game grid class

---
+++ Tetris/GameGrid.cs
@@ -0,0 +1,15 @@
+﻿namespace Tetris;
+
+public class GameGrid
+{
+    private readonly int[,] grid;
+    public int Rows { get; }
+    public int Columns { get; }
+
+    public int this[int r, int c]
+    {
+        get => grid[r, c];
+        set => grid[r, c] = value;
+        //2:39
+    }
+}
